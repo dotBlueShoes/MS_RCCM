2f34eec75886eadc99c8146927150373ed24c528
dotBlueShoes <matthewstrumillo@gmail.com>
2022-09-20 17:16:58 +0200

    Created Tab window

--- MS_RCCM/inc/Windows/Procedures/ProcedureLeft.hpp
+++ MS_RCCM/inc/Windows/Procedures/ProcedureLeft.hpp
@@ -2,7 +2,12 @@
 
 namespace windows::windowLeft {
 	
-	proceeded stdcall WindowLeftProcedure(
+	HWND hTab;
+	
+	#define ID_TAB_ZW 1
+	#define ID_TAB_ST 0
+	
+	proceeded stdcall Procedure(
 		winapi::windowHandle window,
 		input message,
 		winapi::messageW wArgument,
@@ -11,8 +16,30 @@ namespace windows::windowLeft {
 		switch (message) { 
 		
 			case input::Create: {
-				windows::CreateEditor(mainProcess, window);
+				//windows::CreateEditor(mainProcess, window);
+				if constexpr (DEBUG) debug::LogInfo("(CALL) Window-Left:Event-Create");
 				return proceeded::True;
+			}
+				
+			case (winapi::window::input)WM_NOTIFY: {
+				LPNMHDR n = (LPNMHDR)lArgument;
+				if (n->code == TCN_SELCHANGE && n->hwndFrom == hTab) { // Sprawdza, czy uchwyt pochodzi od hTab
+					int index = TabCtrl_GetCurSel(hTab);
+					switch( index ) { // indeks aktualnej kontrolki
+						case ID_TAB_ST:
+							if constexpr (DEBUG) debug::LogInfo("(TABEVENT) ID_TAB_ST");
+							//ShowWindow( hEdit, SW_SHOW );
+							//ShowWindow( hRadioButton, SW_HIDE );
+							break;
+						case ID_TAB_ZW:
+							if constexpr (DEBUG) debug::LogInfo("(TABEVENT) ID_TAB_ZW");
+							//ShowWindow( hEdit, SW_HIDE );
+							//ShowWindow( hRadioButton, SW_SHOW );
+							break;
+							//itd.
+					}
+				}
+				return proceeded::False;
 			}
 			
 			default:
--- MS_RCCM/inc/Windows/WindowLeft.hpp
+++ MS_RCCM/inc/Windows/WindowLeft.hpp
@@ -7,11 +7,12 @@ namespace windows::windowLeft {
 		const winapi::handleInstance& process,
 		const winapi::windowHandle& parentWindow,
 		const winapi::brushHandle& backgroundBrush,
+		const vector2<uint64> & parentOffset,
 		const int32& windowState
 	) {	
 		const array<winapi::wchar, 11> className ( L"WindowLeft" );
 		const vector2<uint64> 
-			windowLeftSize ( 700, 800 ),
+			windowLeftSize ( 700 - parentOffset.x, 800 - parentOffset.y),
 			windowLeftOffset ( 0, 0 );
 			
 		if constexpr (DEBUG) debug::LogInfo("(CALL) Window-Left:Create");
@@ -21,7 +22,7 @@ namespace windows::windowLeft {
 				CreateChildWindow (
 					process, 
 					parentWindow, 
-					(winapi::windowProcedure)WindowLeftProcedure, 
+					(winapi::windowProcedure)Procedure, 
 					(HCURSOR)LoadImage(NULL, MAKEINTRESOURCE(OCR_NORMAL), IMAGE_CURSOR, 0, 0, LR_SHARED),
 					className.Pointer(),
 					backgroundBrush,
@@ -34,6 +35,35 @@ namespace windows::windowLeft {
 			if constexpr (DEBUG)
 				// ERROR HANDLING
 				if (window == NULL) debug::LogError("Window not created!");
+				
+			/// TabControl Kontrolka
+			{
+				// 
+				hTab = CreateWindowExW( 0, WC_TABCONTROLW, 0, WS_CHILD | WS_VISIBLE /*|TCS_FIXEDWIDTH*/,
+					0, 0, 544, 375, window, 0, process, NULL );
+				
+				if constexpr (DEBUG)
+				// ERROR HANDLING
+					if (hTab == NULL) debug::LogError("Window not created!");
+					
+				/// For fixedwidth to set the fixedwidth
+				//SNDMSG(hTab, TCM_SETITEMSIZE, 0, MAKELPARAM(150, 20));
+				
+				TCITEMW tci1, tci2;
+				
+				array<winapi::wchar, 15> text1 ( L"Ten Użytkownik" );
+				array<winapi::wchar, 20> text2 ( L"Wszyscy Użytkownicy" );
+				
+				tci1.mask = TCIF_TEXT;
+				tci2.mask = TCIF_TEXT;
+				tci1.pszText = text1.Pointer();
+				tci2.pszText = text2.Pointer();
+				tci1.cchTextMax = text1.Length();
+				tci2.cchTextMax = text2.Length();
+				
+				SNDMSG(hTab, TCM_INSERTITEMW, (WPARAM)ID_TAB_ZW, (LPARAM)(const TC_ITEMW *)(&tci1));
+				SNDMSG(hTab, TCM_INSERTITEMW, (WPARAM)ID_TAB_ST, (LPARAM)(const TC_ITEMW *)(&tci2));
+			}
 				
 			return window;
 		}
